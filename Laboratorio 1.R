3d115649a17f1f3043682287f4131b0c01aea2f4
AlondraPerales <alondra.zunigap@uanl.edu.mx>
2024-04-14 11:20:46 -0600

    lab. 1

---
+++ Laboratorio 1.R
@@ -0,0 +1,36 @@
+# Laboratorio 1
+# Alondra Perales
+# 14/04/2024
+
+# Gastos totales
+300 + 240 + 1527 + 400 + 1500 + 1833
+
+celular <- 300
+celular
+transporte <- 240
+transporte
+comestibles <- 1527
+comestibles
+gimnasio <- 400
+gimnasio
+alquiler <- 1500
+alquiler
+otros <- 1833
+otros
+gastos <- 5800 #gastos total con la suma de los datos
+gastos
+# Gastos durante semestre escolar
+5800 + 5800 + 5800 + 5800 + 5800
+# Gastos durante un año
+29000 + 29000
+gastos <- c(celular, transporte, comestibles, gimnasio, alquiler, otros)
+barlopt (gastos)
+sort(ylab = "transporte, celular, gimnasio, alquiler, comestibles, otros")
+     xlab = ("gastos")
+sort(barplot)(gastos)
+
+
+
+
+
+
